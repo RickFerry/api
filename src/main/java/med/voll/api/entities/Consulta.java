1f74847685f50533342e73b8bed39899ca78b3c8
RickFerry <rick_ferry1@hotmail.com>
2023-02-23 11:21:31 -0300

    Starter fase 3

---
+++ src/main/java/med/voll/api/entities/Consulta.java
@@ -0,0 +1,23 @@
+package med.voll.api.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Table(name = "consultas")
+@Entity(name = "Consulta")
+public class Consulta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medico_id")
+    private Medico medico;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "paciente_id")
+    private Paciente paciente;
+    private LocalDateTime data;
+}
---
+++ src/main/java/med/voll/api/services/ValidadorCancelamentoDeConsulta.java
@@ -0,0 +1,9 @@
+package med.voll.api.services;
+
+import med.voll.api.domain.consulta.DadosCancelamentoConsulta;
+
+public interface ValidadorCancelamentoDeConsulta {
+
+    void validar(DadosCancelamentoConsulta dados);
+
+}
