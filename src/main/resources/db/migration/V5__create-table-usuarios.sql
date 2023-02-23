efc32d2bfdfa7915328179f9fe455a815f5b001a
RickFerry <rick_ferry1@hotmail.com>
2023-02-22 22:06:37 -0300

    DB mysql add

--- src/main/resources/db/migration/V5__create-table-usuarios.sql
+++ src/main/resources/db/migration/V5__create-table-usuarios.sql
@@ -4,5 +4,4 @@ create table usuarios(
     senha varchar(255) not null
 );
 
-INSERT INTO usuarios (login, senha) VALUES ('ricardo.martins@voll.med', '$2a$12$OYn5VhLUKiOVCHh.j6itj.4jD7UXI2psYC8YnHbBOcKIQj3elFtv2'); --senha(123456)
-INSERT INTO usuarios (login, senha) VALUES ('kalel.martins@voll.med', '$2a$12$n4p.2HtlJSdUT7n2rnR4QOqjqFbW.WinfZdLLhD6hzDpuHQz.zOXG'); --senha(654321)
+insert into usuarios (id, login, senha) values (1, 'kalel.martins@voll.med', '$2a$12$vgn6srbi6jUB0uDgooPDOusdeTQttzu3gY3zuqW2xgpcOIsi999LG');
